aab040a1e2e8c646d7d470009ab5fa96b3d5516a
Ambati Navaneeth <ambatinavaneeth9@gmail.com>
2026-04-20 00:09:24 +0530

    Update problemsolving10.py

--- problemsolving10.py
+++ problemsolving10.py
@@ -9,3 +9,31 @@ else:
             break
     else:
         print(True)
+        
+Print Diagonal Elements
+mat=[[1, 2], [3, 4]]
+n=len(mat)
+res=[]  
+for i in range(n):
+    res.append(mat[i][i])
+print(res) #[[1,4]
+
+Print Anti-Diagonal
+mat=[[1, 2], [3, 4]]
+n=len(mat)
+res=[]
+for i in range(n):
+    res.append(mat[i][n-i-1])
+print(res)  #[2,3]
+
+Printing Non diagonal elements
+matrix = [[1, 2,5],
+          [3, 4,7],
+         [1,2,3]]
+n = len(matrix)
+res = []
+for i in range(n):
+    for j in range(n):
+        if i != j:
+            res.append(matrix[i][j])
+print(res) #[2,5,3,7,1,2]
